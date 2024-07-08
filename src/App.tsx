c88c44ca042e34bab1740ee1cc7ea76ada6daf0e
Ankit Kokane <ankitkokane90@gmail.com>
2024-07-08 14:23:27 +0530

    Created base project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
