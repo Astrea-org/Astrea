768382c8249d82cde98d0bf1215c35b7b9a23d2c
kirtanchandak <themarketingkirtan@gmail.com>
2024-07-10 04:43:12 +0530

    add sections for home page

---
+++ src/components/Explore.tsx
@@ -0,0 +1,67 @@
+import { FaRegHeart } from "react-icons/fa6";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
+function Explore() {
+  return (
+    <>
+      <div className="bg-white pt-12">
+        <div className="my-auto px-5 lg:px-24 gap-4 lg:gap-10 ">
+          <div>
+            <div className="flex justify-between">
+              <h1 className="text-2xl font-bold">Top Datasets</h1>
+              <div className="flex gap-2">
+                <button className="p-2 border border-gray-300">
+                  Show More
+                </button>
+                <div className="flex gap-1">
+                  <button className="px-2 py-1  bg-gray-200">
+                    <IoIosArrowBack />
+                  </button>
+                  <button className="px-2 py-1 bg-gray-200">
+                    <IoIosArrowForward />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="mt-8 pb-12">
+              <div className="bg-white shadow-lg w-52">
+                <img
+                  src="https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  alt=""
+                  className="w-full h-auto "
+                />
+                <div className="flex justify-between px-2">
+                  <p className="p-2 font-semibold">Cats</p>
+                  <span className="mt-2 flex gap-2">
+                    <FaRegHeart className="mt-1" />
+                    324
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h1 className="text-2xl font-bold">Browse by Categories</h1>
+            </div>
+            <div className="mt-12 pb-8">
+              <div className="bg-white shadow-lg w-52">
+                <img
+                  src="https://cdn.britannica.com/34/235834-050-C5843610/two-different-breeds-of-cats-side-by-side-outdoors-in-the-garden.jpg"
+                  alt=""
+                  className="w-full h-auto p-5 aspect-square object-contain"
+                />
+                <div className="flex justify-center items-center text-center bg-gray-200">
+                  <p className="p-2 text-xl font-semibold">Financial</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Explore;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import Explore from "../components/Explore";
 import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black overflow-x-clip">
       <HeroSection />
+      <Explore />
     </div>
   );
 }
