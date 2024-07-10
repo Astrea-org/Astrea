141c7474167a46a89409666e93c6c2d2079abf2f
kirtanchandak <themarketingkirtan@gmail.com>
2024-07-10 15:24:58 +0530

    home page ui polisihing

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import { FaRegHeart } from "react-icons/fa6";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import { CiWallet } from "react-icons/ci";
+import { categories } from "../utils/data/cards";
 
 function Explore() {
+  const [activeAddress, setActiveAddress] = useState<string | null>(null);
+
+  const handleCurrentWallet = async () => {
+    const address = await window.arweaveWallet.getActiveAddress();
+    setActiveAddress(address);
+  };
+
+  const handleConnectWallet = async () => {
+    await window.arweaveWallet.connect(["ACCESS_ADDRESS", "SIGN_TRANSACTION"]);
+    handleCurrentWallet();
+  };
+
   return (
     <>
       <div className="bg-white pt-12">
@@ -44,19 +58,37 @@ function Explore() {
           </div>
 
           <div className="bg-gray-100">
-            <div className="py-32 px-6">
-              <h1 className="text-2xl font-bold">Get started!</h1>
-              <p className="mt-2">
-                Qui ut exercitation officia proident enim non tempor tempor{" "}
-                <br />
-                ipsum ex nulla ea adipisicing sit consequat
-              </p>
-              <button className="border border-gray-400 p-2 mt-4 flex gap-2">
-                Connect Wallet
-                <span>
-                  <CiWallet className="mt-1" size={18} />
-                </span>
-              </button>
+            <div className="py-12 px-28 flex justify-between">
+              <div className="mt-24">
+                <h1 className="text-2xl font-bold">Get started!</h1>
+                <p className="mt-2">
+                  Qui ut exercitation officia proident enim non tempor tempor{" "}
+                  <br />
+                  ipsum ex nulla ea adipisicing sit consequat
+                </p>
+                <button className="border border-gray-400 p-2 mt-4 flex gap-2">
+                  Connect Wallet
+                  <span>
+                    <CiWallet className="mt-1" size={18} />
+                  </span>
+                </button>
+              </div>
+              <div className="flex justify-between">
+                <div className="bg-white shadow-lg w-52">
+                  <img
+                    src="https:/images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                    alt=""
+                    className="w-full h-auto "
+                  />
+                  <div className="flex justify-between px-2">
+                    <p className="p-2 font-semibold">Cats</p>
+                    <span className="mt-2 flex gap-2">
+                      <FaRegHeart className="mt-1" />
+                      324
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
@@ -65,15 +97,25 @@ function Explore() {
               <h1 className="text-2xl font-bold">Browse by Categories</h1>
             </div>
             <div className="mt-12 pb-8">
-              <div className="bg-white shadow-lg w-52">
-                <img
-                  src="https://cdn.britannica.com/34/235834-050-C5843610/two-different-breeds-of-cats-side-by-side-outdoors-in-the-garden.jpg"
-                  alt=""
-                  className="w-full h-auto p-5 aspect-square object-contain"
-                />
-                <div className="flex justify-center items-center text-center bg-gray-200">
-                  <p className="p-2 text-xl font-semibold">Financial</p>
-                </div>
+              <div className="grid grid-cols-1 md:grid-cols-4">
+                {categories.map((category) => (
+                  <a
+                    href="/"
+                    key={category.id}
+                    className="bg-white shadow-lg w-52"
+                  >
+                    <img
+                      src={category.img}
+                      alt="finance"
+                      className="w-full h-auto p-5 aspect-square object-contain"
+                    />
+                    <div className="flex justify-center items-center text-center bg-gray-200">
+                      <p className="p-2 text-xl font-semibold">
+                        {category.name}
+                      </p>
+                    </div>
+                  </a>
+                ))}
               </div>
             </div>
           </div>
---
+++ src/utils/data/cards.ts
@@ -0,0 +1,27 @@
+import finance from "../../assets/finance.avif";
+import health from "../../assets/health.png";
+import geo from "../../assets/geogra.jpg";
+import sensor from "../../assets/sensor.jpg";
+
+export const categories = [
+  {
+    id: 1,
+    img: finance,
+    name: "Finance",
+  },
+  {
+    id: 2,
+    img: health,
+    name: "Health",
+  },
+  {
+    id: 3,
+    img: geo,
+    name: "Geography",
+  },
+  {
+    id: 4,
+    img: sensor,
+    name: "Sensor",
+  },
+];
