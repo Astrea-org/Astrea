a2d170fa2e0ea1e81cd78e39e405b90014602e0b
kirtanchandak <themarketingkirtan@gmail.com>
2024-07-10 10:51:02 +0530

    user connection with ao

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,118 @@
 import { useEffect, useState } from "react";
+import {
+  createDataItemSigner,
+  message,
+  result,
+  dryrun,
+} from "@permaweb/aoconnect";
+import toast from "react-hot-toast";
+
+interface Author {
+  PID: string;
+  name: string;
+}
+
+interface Message {
+  Data: string;
+}
+
+interface DryrunResult {
+  Messages: { Data: string }[];
+}
+
+interface RegisterResult {
+  Messages: Message[];
+}
 
 function Profile() {
   const [activeAddress, setActiveAddress] = useState<string>("");
+  const [isFetching, setIsFetching] = useState(false);
+  const [authorList, setAuthorList] = useState<Author[]>([]);
+  const processId = "FlBYYzcArvs_JnU1lEtiTKgnJOJ9k-3_e3GFjtzgsQI";
 
   const handleCurrentWallet = async () => {
     const address = await window.arweaveWallet.getActiveAddress();
     setActiveAddress(address);
   };
 
+  const syncAllAuthors = async () => {
+    if (!activeAddress) {
+      return;
+    }
+
+    try {
+      const res: DryrunResult = await dryrun({
+        process: processId,
+        data: "",
+        tags: [{ name: "Action", value: "UserList" }],
+        anchor: "1234",
+      });
+      console.log("Dry run Author result", res);
+      const filteredResult: Author[] = res.Messages.map((message) => {
+        const parsedData = JSON.parse(message.Data);
+        return parsedData;
+      });
+      console.log("Filtered Author result", filteredResult);
+      console.log("ok");
+      setAuthorList(filteredResult);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const registerAuthor = async (name: string) => {
+    const toastId = toast.loading("Registering...");
+    try {
+      const res = await message({
+        process: processId,
+        tags: [{ name: "Action", value: "Register" }],
+        data: JSON.stringify({ name }),
+        signer: createDataItemSigner(window.arweaveWallet),
+      });
+
+      console.log("Register Author result", res);
+
+      const registerResult: RegisterResult = await result({
+        process: processId,
+        message: res,
+      });
+
+      console.log("Registered successfully", registerResult);
+
+      if (registerResult.Messages[0].Data === "Successfully Registered.") {
+        await syncAllAuthors();
+        toast.success("Successfully registered!");
+      } else {
+        toast.error("Registration failed.");
+      }
+    } catch (error) {
+      toast.error("Registration failed.");
+      console.log(error);
+    } finally {
+      toast.dismiss(toastId);
+    }
+  };
+
   useEffect(() => {
     handleCurrentWallet();
+  }, []);
+
+  useEffect(() => {
+    if (activeAddress) {
+      setIsFetching(true);
+      syncAllAuthors().finally(() => setIsFetching(false));
+      console.log("This is active address", activeAddress);
+      console.log(
+        "Includes author",
+        authorList.some((author) => author.PID === activeAddress)
+      );
+    }
   }, [activeAddress]);
+
   const kirtan = true;
+
+  console.log(authorList);
+
   return (
     <>
       {kirtan ? (
@@ -53,52 +154,53 @@ function Profile() {
         <>
           <div className="bg-white">
             <div className="max-w-screen-xl px-4 py-8 mx-auto mt-24">
-              <p>
-                <h1 className="font-bold text-2xl">Create Profile</h1>
-                <form action="">
-                  <div className="flex flex-col mt-8">
-                    <label htmlFor="title" className="text-xl font-semibold">
-                      Name*
-                    </label>
-                    <input
-                      id="title"
-                      type="text"
-                      value=""
-                      required
-                      className="w-72 mt-1 rounded-md h-10 border border-gray-500"
-                    />
-                  </div>
-                  <div className="flex flex-col mt-3">
-                    <label htmlFor="title" className="text-xl font-semibold">
-                      Username*
-                    </label>
-                    <input
-                      id="title"
-                      type="text"
-                      value=""
-                      required
-                      className="w-72 mt-1 rounded-md h-10 border border-gray-500"
-                    />
-                  </div>
-                  <div className="flex flex-col mt-3">
-                    <label htmlFor="title" className="text-xl font-semibold">
-                      About
-                    </label>
-                    <input
-                      id="title"
-                      type="text"
-                      value=""
-                      className="w-72 mt-1 rounded-md h-20 border border-gray-500"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="bg-gray-300 px-5 py-2 rounded-lg mt-4"
-                  >
-                    Create
-                  </button>
-                </form>
-              </p>
+              <h1 className="font-bold text-2xl">Create Profile</h1>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  const name = (e.target as any).elements.name.value;
+                  registerAuthor(name);
+                }}
+              >
+                <div className="flex flex-col mt-8">
+                  <label htmlFor="name" className="text-xl font-semibold">
+                    Name*
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    required
+                    className="w-72 mt-1 rounded-md h-10 border border-gray-500"
+                  />
+                </div>
+                <div className="flex flex-col mt-3">
+                  <label htmlFor="username" className="text-xl font-semibold">
+                    Username*
+                  </label>
+                  <input
+                    id="username"
+                    type="text"
+                    required
+                    className="w-72 mt-1 rounded-md h-10 border border-gray-500"
+                  />
+                </div>
+                <div className="flex flex-col mt-3">
+                  <label htmlFor="about" className="text-xl font-semibold">
+                    About
+                  </label>
+                  <input
+                    id="about"
+                    type="text"
+                    className="w-72 mt-1 rounded-md h-20 border border-gray-500"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="bg-gray-300 px-5 py-2 rounded-lg mt-4"
+                >
+                  Create
+                </button>
+              </form>
             </div>
           </div>
         </>
